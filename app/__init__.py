12ca50b447a43e97200ad8f7b055e40717ff0db2
Paolo Villanueva <nv.paolo@gmail.com>
2026-06-18 14:47:19 -0500

    Add readme y migracion de bd

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,14 @@ from flask import Flask, redirect, url_for
 
 from app.config import Config
 from app.controllers import auth_bp, usuarios_bp
-from app.extensions import db
-
+from app.extensions import db, migrate
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(usuarios_bp)
